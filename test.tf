cf2e575cb70ec4d717dca3c49978cff72bdf51b2
andovnar2021 <87485716+andovnar2021@users.noreply.github.com>
2022-05-01 22:59:41 +0300

    Update test.tf

--- test.tf
+++ test.tf
@@ -14,7 +14,7 @@ module "aws_vpc" {
 module "aws_sg" {
 
     source  = "github.com/andovnar2021/terraform-modules/aws_security_group"
-    in_list_ports = [{from_port = 8080, to_port = 8080, protocol    = "tcp",cidr_blocks = ["0.0.0.0/0"]},{from_port = 80, to_port = 80, protocol    = "tcp",cidr_blocks = ["10.1.0.0/16"]}]
+    #in_list_ports = [{from_port = 8080, to_port = 8080, protocol    = "tcp",cidr_blocks = ["0.0.0.0/0"]},{from_port = 80, to_port = 80, protocol    = "tcp",cidr_blocks = ["10.1.0.0/16"]}]
 
 }
 
